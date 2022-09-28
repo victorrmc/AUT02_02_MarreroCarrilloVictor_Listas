8855c73a160c7c65720e9565696520b984cfc9be
victo <victo@KUCUH>
2022-09-28 09:05:01 +0100

    Agregar archivos de proyecto.

---
+++ AUT02_02_MarreroCarrilloVictor_Listas/Controllers/PersonajeController.cs
@@ -0,0 +1,27 @@
+﻿using AUT02_02_MarreroCarrilloVictor_Listas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+
+namespace AUT02_02_MarreroCarrilloVictor_Listas.Controllers
+{
+    public class PersonajeController : Controller
+    {
+        public static List<Models.Personaje> perso = new List<Models.Personaje>() {
+
+        new Models.Personaje() { id = 1, name = "Philp Dunphy", family = "Dunphy", nchildren = 3 },
+        new Models.Personaje() { id = 2, name = "Luke Dunphy", family = "Dunphy", nchildren = 0 }
+
+        };
+        
+        
+       
+        public IActionResult Index()
+        {
+            return View(perso);
+        }
+    }
+}
---
+++ AUT02_02_MarreroCarrilloVictor_Listas/Models/Personaje.cs
@@ -0,0 +1,10 @@
+﻿namespace AUT02_02_MarreroCarrilloVictor_Listas.Models
+{
+    public class Personaje
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string family { get; set; }
+        public int nchildren { get; set; }
+    }
+}
